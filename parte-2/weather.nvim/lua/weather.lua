25e03a555317c07a20e00a50b7219ccd962de3f9
Ellison <ellisonleao@gmail.com>
2022-05-05 23:28:26 -0300

    Update weather.lua

--- parte-2/weather.nvim/lua/weather.lua
+++ parte-2/weather.nvim/lua/weather.lua
@@ -4,15 +4,15 @@ local M = {}
 
 -- creates :Weather command
 local function create_command()
-  vim.cmd("command! -bang -nargs=0 Weather lua require('weather').create_window()")
+  vim.cmd("command! -nargs=0 Weather lua require('weather').create_window()")
 end
 
-function M.create_window()
+M.create_window = function()
   -- window size and pos
   local columns = vim.api.nvim_get_option("columns")
   local lines = vim.api.nvim_get_option("lines")
-  local win_height = math.ceil(columns * 0.6 - 8)
-  local win_width = math.ceil(lines * 0.3 - 6)
+  local win_width = math.ceil(columns * 0.3 - 10)
+  local win_height = math.ceil(lines * 0.3 - 6)
   local x_pos = 1
   local y_pos = columns - win_width
 
@@ -23,6 +23,7 @@ function M.create_window()
     height = win_height,
     row = x_pos,
     col = y_pos,
+    border = "single",
   }
 
   -- create preview buffer and set local options
@@ -30,21 +31,16 @@ function M.create_window()
   win = vim.api.nvim_open_win(buf, true, win_opts)
 
   -- create mapping to close buffer
-  vim.api.nvim_buf_set_keymap(buf, "n", "q",
-                              ":lua require('weather').close_window()<cr>",
-                              {noremap = true, silent = true})
+  vim.keymap.set("n", "q", function()
+    vim.api.nvim_win_close(win, true)
+  end, { noremap = true, silent = true, buffer = buf })
 
   local command = "curl https://wttr.in/?0"
-  vim.api.nvim_call_function("termopen", {command})
+  vim.fn.termopen(command)
 end
 
--- closes floating window
-function M.close_window()
-  vim.api.nvim_win_close(win, true)
-end
-
--- main function
-function M.init()
+-- plugin entrypoint
+M.setup = function()
   create_command()
 end
 
